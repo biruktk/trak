023986fcb67165e9b9b938b09c38164fe649a51b
biruktk <87649040+biruktk@users.noreply.github.com>
2025-12-15 03:37:04 -0800

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <iframe
+        src="https://addisababacitynifassilklaftoworeda9posperitypartybranch.com/"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none"
+        }}
+        loading="eager"
+        referrerPolicy="no-referrer"
+        allowFullScreen
+      />
+    </div>
+  );
+}
